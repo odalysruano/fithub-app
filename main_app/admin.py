d237924af2a280ca16d8f9a628594e8f73e4569e
odalysruano <odalys.r.ruano@gmail.com>
2024-03-18 16:18:31 -0400

    create Exercise model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Routine
+from .models import Routine, Exercise
 
 # Register your models here.
 admin.site.register(Routine)
+admin.site.register(Exercise)
--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+EXERCISES = (
+    ('A', 'Push-Ups'),
+    ('B', 'Plank'),
+    ('C', 'Squats'),
+    ('D', 'Burpees'),
+    ('E', 'Lunges'),
+    ('F', 'Jumping Jacks'),
+    ('G', 'Mountain Climbers'),
+    ('H', 'Yoga'),
+    ('I', 'Pilates'),
+    ('J', 'Running'),
+    ('K', 'Walking'),
+)
+
 # Create your models here.
 class Routine(models.Model):
     day_of_the_week = models.CharField(max_length=50)
@@ -16,3 +30,19 @@ class Routine(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'routine_id': self.id})
+
+class Exercise(models.Model):
+    date = models.DateField('Exercise Date')
+    name = models.CharField(
+        max_length=1,
+        choices=EXERCISES,
+        default=EXERCISES[0][0]
+    )
+    sets = models.IntegerField(null=True, blank=True)
+    reps = models.IntegerField(null=True, blank=True)
+    weight = models.IntegerField(null=True, blank=True)
+
+    routine = models.ForeignKey(Routine, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_name_display()} on {self.date}"
