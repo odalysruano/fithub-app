69fe8f6f9095fada188a9ecb137391e124bfbda2
odalysruano <odalys.r.ruano@gmail.com>
2024-03-18 11:55:35 -0400

    implement signup functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Routine
 
@@ -10,7 +13,21 @@ def about(request):
     return render(request, 'about.html')
 
 def routines_index(request):
-    routines = Routine.objects.all()
+    routines = Routine.objects.filter(user=request.user)
     return render(request, 'routines/index.html', {
         'routines': routines
     })
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
