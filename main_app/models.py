af5507899e0834e4e9233ade711fd6abbdddcfa6
odalysruano <odalys.r.ruano@gmail.com>
2024-03-18 11:41:47 -0400

    implement login/logout functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Routine(models.Model):
@@ -6,6 +7,8 @@ class Routine(models.Model):
     focus_area = models.CharField(max_length=50)
     time_goal = models.IntegerField()
     calorie_goal = models.IntegerField()
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.day_of_the_week
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('routines/', views.routines_index, name='routines'),
+    path('routines/', views.routines_index, name='index'),
 ]
