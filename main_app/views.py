6e595dcd778246e60355c969afe5694ae244f7a9
odalysruano <odalys.r.ruano@gmail.com>
2024-03-18 10:22:34 -0400

    add functionality to view all routines

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+routines = [
+    {'day_of_the_week': 'monday', 'focus_area': 'arms', 'time_goal': 30, 'calories_goal': 200},
+    {'day_of_the_week': 'tuesday', 'focus_area': 'legs', 'time_goal': 45, 'calories_goal': 250}
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def routines_index(request):
+    return render(request, 'routines/index.html', {
+        'routines': routines
+    })
