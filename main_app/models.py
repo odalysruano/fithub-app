87ce9a4af4e32c58f83dd5605568319fb92c7d9d
odalysruano <odalys.r.ruano@gmail.com>
2024-03-18 10:39:00 -0400

    create and import Routine model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Routine(models.Model):
+    day_of_the_week = models.CharField(max_length=50)
+    focus_area = models.CharField(max_length=50)
+    time_goal = models.IntegerField()
+    calorie_goal = models.IntegerField()
+
+    def __str__(self):
+        return self.day_of_the_week
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+from .models import Routine
+
 routines = [
-    {'day_of_the_week': 'monday', 'focus_area': 'arms', 'time_goal': 30, 'calories_goal': 200},
-    {'day_of_the_week': 'tuesday', 'focus_area': 'legs', 'time_goal': 45, 'calories_goal': 250}
+    {'day_of_the_week': 'monday', 'focus_area': 'arms', 'time_goal': 30, 'calorie_goal': 200},
+    {'day_of_the_week': 'tuesday', 'focus_area': 'legs', 'time_goal': 45, 'calorie_goal': 250}
 ]
 
 # Create your views here.
@@ -13,6 +15,7 @@ def about(request):
     return render(request, 'about.html')
 
 def routines_index(request):
+    routines = Routine.objects.all()
     return render(request, 'routines/index.html', {
         'routines': routines
     })
