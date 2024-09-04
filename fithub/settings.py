65e340c3546a710163ec4ca5a882f7c854aa2e96
odalysruano <odalys.r.ruano@gmail.com>
2024-09-03 20:12:28 -0400

    modify settings.py

--- fithub/settings.py
+++ fithub/settings.py
@@ -123,8 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 LOGIN_REDIRECT_URL = '/routines/'
 
 LOGOUT_REDIRECT_URL = '/'
